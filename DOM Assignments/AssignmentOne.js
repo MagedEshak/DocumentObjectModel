45d0c56de5f46f03370d52f96e56ebf0b1424cd2
Maged Eshak <magedeshak39@gmail.com>
2025-02-27 16:33:46 +0200

    assignmentOne

--- DOM Assignments/AssignmentOne.js
+++ DOM Assignments/AssignmentOne.js
@@ -1,7 +1,28 @@
-onsole.log("----------------------");
+console.log("----------------------");
 console.log("-- حل التكليفات --");
 console.log("----------------------");
 
-
 console.log("-- التكليف 01 --");
 console.log("----------------------");
+
+console.log(document.getElementById("elzero"));
+console.log(document.getElementsByClassName("element")[0]);
+console.log(document.getElementsByTagName("div")[0]);
+console.log(document.getElementsByName("js")[0]);
+console.log(document.querySelector("#elzero"));
+console.log(document.querySelector(".element"));
+console.log(document.querySelector("div"));
+console.log(document.querySelector("[name = 'js']"));
+console.log(document.querySelectorAll(".element")[0]);
+console.log(document.querySelectorAll("#elzero")[0]);
+console.log(document.querySelectorAll("[name = 'js']")[0]);
+console.log(document.querySelectorAll("div")[0]);
+console.log(document.body.firstElementChild);
+console.log(document.body.childNodes[3]);
+
+
+
+
+
+
+

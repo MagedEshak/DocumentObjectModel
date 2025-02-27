4803e725439a7819a032fb8ec3a9b5c6536307f6
Maged Eshak <magedeshak39@gmail.com>
2025-02-27 12:56:41 +0200

    up

--- ClassListAndCSSStyling.js
+++ ClassListAndCSSStyling.js
@@ -1,5 +1,5 @@
 console.log("-".repeat(20));
-console.log("6- ClassList Object And Methods");
+console.log("2- ClassList Object And Methods");
 console.log("-".repeat(20));
 
 /*
@@ -26,7 +26,7 @@ element.onclick = function () {
 };
 
 console.log("-".repeat(20));
-console.log("6- CSS Styling And Stylesheet");
+console.log("3- CSS Styling And Stylesheet");
 console.log("-".repeat(20));
 
 
@@ -42,4 +42,27 @@ element.style.removeProperty("background-color");
 console.log(document.styleSheets[0].cssRules[0].style.removeProperty("line-height"));
 console.log(document.styleSheets[0].cssRules[0].style.setProperty("color", "blue"));
 
+console.log("-".repeat(20));
+console.log("4- Before, After, Prepend, Append, Remove");
+console.log("-".repeat(20));
+
+/*
+    DOM [Deal With Elements]
+    - before [Element || String]
+    - after [Element || String]
+    - append [Element || String]
+    - prepend [Element || String]
+    - remove
+*/
+
+let ele = document.getElementById("my-sDiv");
+let createP = document.createElement("p");
+
+createP.append("This a Paragraph");
+
+ele.before(createP); //add element in before element
+ele.after(createP); // add element in after element
+ele.append(" End"); // add element in the end of element
+ele.prepend("HI "); // add element in the top of element
+ele.remove(); // remove completely from document
 

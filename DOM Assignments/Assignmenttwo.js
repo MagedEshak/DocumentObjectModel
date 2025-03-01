05ea5bb15343f17ae0eaf3cf80e0b29b7ee91cd2
Maged Eshak <magedeshak39@gmail.com>
2025-03-01 18:37:36 +0200

    Assignment two solved

--- DOM Assignments/Assignmenttwo.js
+++ DOM Assignments/Assignmenttwo.js
@@ -1,3 +1,13 @@
 console.log("-- التكليف 02 --");
 console.log("----------------------");
 
+let img = document.querySelectorAll("img");
+
+console.log(img);
+
+img.forEach(function (ele) {
+    ele.setAttribute("src", "https://elzero.org/wp-content/themes/elzero/imgs/logo.png");
+    ele.setAttribute("alt", "Elzero Logo");
+});
+
+document.body.style.backgroundColor = "black";
